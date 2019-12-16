80cb4ec79b9c753c063729516ab55b27e6b5523a
Jonathan Wood <jonathan.m.wood95@gmail.com>
2019-12-16 21:59:27 +0000

    Moved Vec2 to it's own file

--- main.cpp
+++ main.cpp
@@ -6,40 +6,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
-
-struct Vec2{
-    inline void operator +=(const Vec2& B) {
-        x += B.x;
-        y += B.y;
-    }
-
-    inline bool operator == (const Vec2& B) const {
-        return x == B.x && y == B.y;
-    }
-
-    inline bool operator != (const Vec2& B) const {
-        return x != B.x  ||  y != B.y;
-    }
-
-    inline Vec2 operator +(const Vec2& B) const {
-        return Vec2{x + B.x, y + B.y};
-    }
-
-    inline Vec2 operator -(const Vec2& B) const {
-        return Vec2{x - B.x, y - B.y};
-    }
-
-    inline Vec2 inverse() const {
-        return Vec2{-x, -y};
-    }
-
-    std::string str() const {
-        return "(" + std::to_string(x) + "," + std::to_string(y) + ")";
-    }
-
-    int x = 0;
-    int y = 0;
-};
+#include "Vec2.h"
 
 
 // It is assumed that a snake always has at least 3 segments
